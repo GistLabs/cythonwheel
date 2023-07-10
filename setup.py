c3998d2ca74a512eca61762c4193c7f92b80d3e9
John Heintz <john@gistlabs.com>
2023-07-09 23:35:23 -0500

    Add setup.py based cython

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from Cython.Build import cythonize
+from setuptools.dist import Distribution
+import tomllib
+
+
+with open("pyproject.toml", "rb") as f:
+    data = tomllib.load(f)
+
+class BinaryDistribution(Distribution):
+    def is_pure(self):
+        return False
+
+
+setup(
+    name=data['tool']['poetry']['name'],
+    version=data['tool']['poetry']['version'],
+    author=data['tool']['poetry']['authors'][0].split()[0],
+    author_email=data['tool']['poetry']['authors'][0].split()[1],
+    url='https://gistlabs.com',
+    install_requires=['numpy'],
+    ext_modules=cythonize(
+        "cythonwheel/**/*.py",
+        compiler_directives={'language_level': 3, "linetrace": False},
+    ),
+    include_package_data=True,
+    distclass=BinaryDistribution, )
